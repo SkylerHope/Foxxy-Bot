d3fb26c56c92f6602dd56e5cfb82f1bee8dbbe5a
SkylerHope <elpidakouro13@gmail.com>
2024-01-08 13:04:41 +0200

    Adding conditions if user is none

--- commands/utility/userinfo.js
+++ commands/utility/userinfo.js
@@ -13,8 +13,16 @@ module.exports = {
                 .setRequired(false)
     ),
     execute: async (interaction) => {
-        const mentionedUser = interaction.options.getName('user');
+        const author = message.author;
+        const user = interaction.options.getUser('user');
+        if (!user) {
+            user = author;
+        }
+
+        const userInfoEmbed = new EmbedBuilder()
+            .setTitle('Info on ' + user.username)
+
         // Below line for testing
-        await interaction.reply(mentionedUser); 
+        await interaction.reply(userInfoEmbed); 
     }
 };
