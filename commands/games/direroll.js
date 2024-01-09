a2cab89d841c33d67b0ac90091e0c7e2dfbcb85c
SkylerHope <elpidakouro13@gmail.com>
2024-01-09 13:36:50 +0200

    Minor adjustments, diceroll command ready

--- commands/games/direroll.js
+++ commands/games/direroll.js
@@ -9,6 +9,6 @@ module.exports = {
     execute: async (interaction) => {
         const numbers = [1, 2, 3, 4, 5, 6];
         let number = numbers[Math.floor(Math.random() * numbers.length)];
-        await interaction.reply(`You got a ${number}`);
+        await interaction.reply(`You got ${number}!`);
     },
 };
