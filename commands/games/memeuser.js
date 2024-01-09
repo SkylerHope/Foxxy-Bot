b12ca8db8984982885a73f3aba67da72e28668cd
SkylerHope <elpidakouro13@gmail.com>
2024-01-09 16:28:05 +0200

    memeuser command tested, added to stable branch

---
+++ commands/games/memeuser.js
@@ -0,0 +1,36 @@
+// memeuser.js
+
+const { SlashCommandBuilder } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('memeuser')
+        .setDescription('Meme a user')
+        .addMentionableOption(option =>
+            option.setName('user')
+                .setDescription('Mention a user')
+                .setRequired(true)
+    ),
+    execute: async (interaction) => {
+        const author = interaction.user;
+        let authorAvatar = author.avatarURL({ format: 'png', dynamic: true, size: 32 });
+        let user = interaction.options.getUser('user');
+        const userName = user.username;
+        const userMention = user.mention;
+        let userAvatar = user
+            ? user.avatarURL({ format: 'png', dynamic: true, size: 32 }) || author.avatarURL({ format: 'png', dynamic: true, size: 32 })
+            : author.avatarURL({ format: 'png', dynamic: true, size: 32 });
+
+        if(userAvatar == authorAvatar && user != author) {
+                userAvatar = "https://static.wikia.nocookie.net/among-us-wiki/images/5/54/Seeker.png";
+        }
+
+        const memeUserEmbed = new EmbedBuilder()
+            .setTitle(`${userName}`)
+            .setDescription(`You done did it now <@${user.id}>!`)
+            .setThumbnail(userAvatar)
+            .setImage("https://i.ibb.co/724F7wZ/laughatthisuser.gif")
+        await interaction.reply({ embeds: [memeUserEmbed] });
+    },
+};
