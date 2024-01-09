23e4f1c40950dabc5af91a1f745c166d37aaaf38
SkylerHope <elpidakouro13@gmail.com>
2024-01-09 13:38:23 +0200

    diceroll command tested and added to stable branch

---
+++ commands/games/direroll.js
@@ -0,0 +1,14 @@
+// diceroll.js
+
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('diceroll')
+        .setDescription('Roll the dice!'),
+    execute: async (interaction) => {
+        const numbers = [1, 2, 3, 4, 5, 6];
+        let number = numbers[Math.floor(Math.random() * numbers.length)];
+        await interaction.reply(`You got ${number}!`);
+    },
+};
