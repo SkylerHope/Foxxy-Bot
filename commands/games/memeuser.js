e3335965d65466c571be48a3976a8087409ca582
SkylerHope <elpidakouro13@gmail.com>
2024-01-09 16:23:58 +0200

    Decresed size of pfps in the favor of responsiveness on mobile devices

--- commands/games/memeuser.js
+++ commands/games/memeuser.js
@@ -14,13 +14,13 @@ module.exports = {
     ),
     execute: async (interaction) => {
         const author = interaction.user;
-        let authorAvatar = author.avatarURL({ format: 'png', dynamic: true, size: 512 });
+        let authorAvatar = author.avatarURL({ format: 'png', dynamic: true, size: 32 });
         let user = interaction.options.getUser('user');
         const userName = user.username;
         const userMention = user.mention;
         let userAvatar = user
-            ? user.avatarURL({ format: 'png', dynamic: true, size: 512 }) || author.avatarURL({ format: 'png', dynamic: true, size: 512 })
-            : author.avatarURL({ format: 'png', dynamic: true, size: 512 });
+            ? user.avatarURL({ format: 'png', dynamic: true, size: 32 }) || author.avatarURL({ format: 'png', dynamic: true, size: 32 })
+            : author.avatarURL({ format: 'png', dynamic: true, size: 32 });
 
         if(userAvatar == authorAvatar && user != author) {
                 userAvatar = "https://static.wikia.nocookie.net/among-us-wiki/images/5/54/Seeker.png";
@@ -30,7 +30,7 @@ module.exports = {
             .setTitle(`${userName}`)
             .setDescription(`You done did it now <@${user.id}>!`)
             .setThumbnail(userAvatar)
-            .setImage("https://i.ibb.co/724F7wZ/laughatthisuser.gif");
+            .setImage("https://i.ibb.co/724F7wZ/laughatthisuser.gif")
         await interaction.reply({ embeds: [memeUserEmbed] });
     },
 };
