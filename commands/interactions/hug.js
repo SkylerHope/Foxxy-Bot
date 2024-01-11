3562712894356cbc76fa581576706ded08c0fd1c
SkylerHope <elpidakouro13@gmail.com>
2024-01-11 13:46:26 +0200

    Hug command tested, added to stable branch

---
+++ commands/interactions/hug.js
@@ -0,0 +1,27 @@
+// hug.js
+
+const { SlashCommandBuilder } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
+const { hugGifs } = require('../../gifs.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('hug')
+        .setDescription('Hug a user')
+        .addMentionableOption(option =>
+            option.setName('user')
+                .setDescription('Mention a user')
+                .setRequired(true)
+    ),
+    execute: async (interaction) => {
+        const author = interaction.user;
+        const authorName = author.username;
+        let user = interaction.options.getUser('user');
+        let gif = hugGifs[Math.floor(Math.random() * hugGifs.length)];
+
+        const hugEmbed = new EmbedBuilder()
+            .setDescription(`${authorName} is hugging <@${user.id}>`)
+            .setThumbnail(gif)
+        await interaction.reply({ embeds: [hugEmbed] });
+    },
+};
